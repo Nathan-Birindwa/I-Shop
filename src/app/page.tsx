a6f204ba962c2f2e85460674a4af2e85a326f692
Nathan-Birindwa <birhindwan@gmail.com>
2025-05-30 14:22:58 +0300

    Added promotion label and category section (partial)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Nav from "@/components/Nav";
 import dynamic from "next/dynamic";
+import PromotionNav from "@/components/PromotionNav";
+import Categories from "@/components/Categories";
 
 const CarouselComponent = dynamic(() => import("../components/Carousel"), {
   ssr: false,
@@ -9,13 +11,16 @@ const CarouselComponent = dynamic(() => import("../components/Carousel"), {
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
+      <PromotionNav />
       <Nav />
 
       {/* Main content area */}
-      <main className="relative">
+      <main className="relative z-0">
+        <Categories />
+
         {/* Carousel section */}
         <section className="w-full h-[600px] md:h-[600px] lg:h-[500px] overflow-hidden">
-          <CarouselComponent />
+          {/* <CarouselComponent /> */}
         </section>
 
         <section className="container mx-auto px-4 py-8"></section>
